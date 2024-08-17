959bdca6b5e55647a99ce7bc6b7c38dbf0018a62
Serigne-Mbaye <104200342+Serigne-Mbaye@users.noreply.github.com>
2024-08-17 14:00:49 +0200

    Unused Variable Warning / Outdated caniuse-lite Database /

--- src/components/FadeInSection.js
+++ src/components/FadeInSection.js
@@ -3,6 +3,47 @@ import React from "react";
 export default function FadeInSection(props) {
   const [isVisible, setVisible] = React.useState(false);
   const domRef = React.useRef();
+
+  React.useEffect(() => {
+    const currentRef = domRef.current; // Store the current value of domRef
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setVisible(entry.isIntersecting);
+        }
+      });
+    });
+
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    // Cleanup function
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? "is-visible" : ""}`}
+      style={{ transitionDelay: `${props.delay}` }}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+/*
+import React from "react";
+
+export default function FadeInSection(props) {
+  const [isVisible, setVisible] = React.useState(false);
+  const domRef = React.useRef();
   React.useEffect(() => {
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
@@ -24,3 +65,4 @@ export default function FadeInSection(props) {
     </div>
   );
 }
+  */
